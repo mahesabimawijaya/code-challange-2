f0100b5294039341fdc3df37fb68a65acf72d497
mahesabimawijaya <mahesabw26@gmail.com>
2023-12-09 01:00:23 +0700

    debug warn & error

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -30,7 +30,7 @@ export default function Marquee() {
         </li>
       </ul>
       <ul
-        class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
         aria-hidden="true"
       >
         <li>
