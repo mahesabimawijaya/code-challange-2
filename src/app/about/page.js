9d58d2e18ea3531f92ec757244e66996e266bbd3
mahesabimawijaya <mahesabw26@gmail.com>
2023-12-12 12:37:41 +0700

    fix overflow-x on teams page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -18,10 +18,10 @@ export default function About() {
             About Us
           </div>
         </div>
-        <div className="flex w-full justify-center md:justify-start text-xl md:text-3xl font-semibold my-10 md:pl-24">
+        <div className="flex w-full justify-center md:justify-start text-xl md:text-3xl font-semibold my-10 md:pl-24 2xl:pl-72">
           <div>Introduction</div>
         </div>
-        <div className=" flex flex-col items-center md:items-start space-y-16 md:flex md:flex-row w-full md:px-24 md:space-x-24">
+        <div className=" flex flex-col items-center md:items-start space-y-16 md:space-y-0 md:flex md:flex-row w-full md:px-24 md:space-x-24 2xl:space-x-72 2xl:px-72">
           <div className="w-1/2 text-justify">
             PT. ASABA Digital Innotech berdiri pada tahun 1990 dengan misi
             menciptakan nilai bisnis bagi pelanggan melalui pemberian solusi
@@ -38,7 +38,7 @@ export default function About() {
             Innotech). Perusahaan kami telah bertransformasi model bisnis dari
             Konsultan IT menjadi Platform Bisnis Digital
           </div>
-          <div className="relative w-52 md:w-1/3 h-32 md:h-36 lg:h-44 xl:h-64">
+          <div className="relative w-52 md:w-1/3 h-32 md:h-36 lg:h-44 xl:h-64 2xl:w-1/5 2xl:ml-20">
             <Image src={"/who-are-we.webp"} fill={true} alt="intro" />
           </div>
         </div>
@@ -65,10 +65,9 @@ export default function About() {
               Mission
             </div>
             <div className=" text-sm md:text-base mt-5 px-5 text-justify">
-              Become a global and trusted digital transformation partner,
-              providing services to enable business partner to accelerate
-              business innovation and win the competition in new economic
-              paradigm.
+              Democratizes quality, secure, and reliable services to enable the
+              adoption of digital technology for all business partners quickly,
+              easily, and effectively.
             </div>
           </div>
         </div>
--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -30,20 +30,20 @@ export default function Teams() {
   return (
     <>
       <StaticNavbar />
-      <div className="pt-16 flex flex-col w-screen h-screen overflow-x-hidden">
+      <div className="pt-16 flex flex-col w-full h-full overflow-x-hidden">
         <div className=" text-xl lg:text-3xl font-semibold text-center">
           Meet Our Teams
         </div>
         <div className="text-sm lg:text-lg text-center">
           Best Programmers from Da West
         </div>
-        <div className="flex flex-col lg:flex lg:flex-row w-full justify-center items-center mt-14 lg:space-x-2 xl:space-x-5 lg:flex-wrap">
+        <div className="flex flex-col lg:flex lg:flex-row w-full justify-center items-center mt-14 lg:space-x-2 xl:space-x-5 2xl:space-x-14 2xl:px-[20%] lg:flex-wrap">
           {data.results.map((user, index) => {
             return (
               <div
                 key={index}
                 id="user-container"
-                className="w-64 lg:w-1/5 border rounded-t-full border-blue-700 h-80 flex flex-col py-8 px-2 items-center mb-5 group hover:bg-blue-800 duration-300 cursor-help"
+                className="w-64 lg:w-1/5 2xl:w-72 border rounded-t-full border-blue-700 h-80 flex flex-col py-8 px-2 items-center mb-5 group hover:bg-blue-800 duration-300 cursor-help"
               >
                 <div className="relative w-40 h-40 mb-4">
                   <Image
